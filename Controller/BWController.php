60a17c6efc295bcc86c79b55e19a70fd19011f2d
Victor <bocharsky.bw@gmail.com>
2013-12-03 12:34:47 +0200

    Init Main Bundle

---
+++ Controller/BWController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace BW\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use BW\MainBundle\Service\PropertyOverload;
+
+class BWController extends Controller {
+    
+
+    /**
+     * @global \Symfony\Component\HttpFoundation\Request $request
+     * @global \Doctrine\ORM\EntityManager $em Instance of EntityManager class
+     * @global \Doctrine\DBAL\Connection $conn
+     */
+    public function __construct() {
+    }
+    
+    
+    /**
+     * Get instance of PropertyOverload class for dynemic properties
+     * @return \BW\Helpers\PropertyOverload\PropertyOverload
+     */
+    public function getPropertyOverload() {
+
+        return new PropertyOverload();
+    }
+    
+    public function render($view, array $parameters = array(), \Symfony\Component\HttpFoundation\Response $response = null) {
+        
+        return parent::render($view, $parameters, $response);
+    }
+}
